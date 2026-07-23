e9cf7f416bf4005ab70623431015695767d50eda
PrashantDhiman005 <prashantdhiman104@gmail.com>
2026-07-23 11:43:18 +0530

    Initial Commit

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,6 +3,7 @@ import Hero from "../components/landing/Hero";
 import AIShowcase from "../components/landing/AIShowcase";
 import Features from "../components/landing/Features";
 import Testimonials from "../components/landing/Testimonials";
+import Pricing from "../components/landing/Pricing";
 import FAQ from "../components/landing/FAQ";
 import Footer from "../components/landing/Footer";
 
@@ -14,6 +15,7 @@ export default function Landing() {
       <AIShowcase />
       <Features />
       <Testimonials />
+      <Pricing />
       <FAQ />
       <Footer />
     </div>
